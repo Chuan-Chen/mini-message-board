a66fd75e47b7d4bef75a6e56db3113a16d0d87b5
Whykari <61473858+Chuan-Chen@users.noreply.github.com>
2024-02-27 17:34:13 -0500

    added some updates to mini-messageboard

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ const postRoute = require('./src/router/postRoute.js')
 const port = 3000;
 const app = express();
 const posts = require('./src/data/messages.js');
+const emitter = require('./src/emitters/emitter.js');
 
 const CERT_DIR = './cert'
 
@@ -16,20 +17,33 @@ app.set('views', './src/views');
 
 app.use('/', postRoute);
 
+/**
+
+
+
+
 app.get('/stream', async (req, res) => {
-    console.log("data requested.")
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.flushHeaders();
-    console.log("posting data")
-    res.write(`data: ${JSON.stringify(await posts.get())} \n\n`);
-    req.on('close', () => {
-        res.end();
-    })
+    try{
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+        })
+    
+        console.log("posting data")
+        
+        res.write(`data: ${JSON.stringify(await posts.get())} \n\n`)
+        req.on('close', () => {
+            res.end();
+        })
+    }catch(err){
+        res.status(404).json({message: err})
+    }
+
 })
 
 
+ */
 /*
 const server = spdy.createServer({
     key: fs.readFileSync(`${CERT_DIR}/server.key`),
--- app/src/controller/postController.js
+++ app/src/controller/postController.js
@@ -1,7 +1,9 @@
-const posts = require('../data/messages')
+const posts = require('../data/messages');
+const emitter = require('../emitters/emitter.js');
 
 exports.newPost = async (req, res) => {
     try{
+        const postdata = await posts.get();
         const post = {
             title: req.query.title,
             user: req.query.user,
@@ -9,9 +11,11 @@ exports.newPost = async (req, res) => {
             message: req.query.message
         }
         posts.push(post);
+        emitter.emit('data', post);
         //res.status(200).json({message: 'added post successfully', data: post});
         res.redirect("/");
         
+        //res.render('index', {title: 'Mini-Messageboard', message: 'Posts', items: post, NumberOfPosts: 0});
     }catch(err){
         console.log(err);
         res.status(400).json({error: err});
@@ -21,11 +25,14 @@ exports.newPost = async (req, res) => {
 exports.getPost = async (req, res) => {
     try{
         const postdata = await posts.get();
-        //console.log(postdata.data)
-        res.render('index', {title: 'Mini-Messageboard', message: 'Posts', items: postdata.data, NumberOfPosts: postdata.data.length});
-        
+        console.log(postdata.data)
+        renderer(res, postdata.data);
     }catch(err){
         console.log(err);
         res.status(400).json({error: err});
     }
+}
+
+const renderer = async(res, data) => {
+    res.render('index', {title: 'Mini-Messageboard', message: 'Posts', items: data, NumberOfPosts: data.length})
 }
